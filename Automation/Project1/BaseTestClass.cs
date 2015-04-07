c32047c5e196315e88be1ab66f68712817f56e24
Jaya Dinu <jdinu@midwire.org>
2015-04-07 12:32:46 -0400

    Addded Selenium WebDriver ,Selenium WebDriver Support classes, Nunit and Nunit Test Adaptor via  Manage NuGet Packages Added the BaseTestClass  with setUp and tearDown Method Test Fixture

---
+++ Automation/Project1/BaseTestClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenQA.Selenium;
+//using OpenQA.Selenium.Chrome;
+ using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+
+namespace Project1
+{
+    public class BaseTestClass
+    {
+
+        protected IWebDriver m_driver;
+
+
+
+        [SetUp]
+        public void setUp()
+        {
+            m_driver = new FirefoxDriver();
+        }
+
+        [TearDown]
+        public void tearDown()
+        {
+            m_driver.Close();
+            m_driver.Dispose();
+
+
+        }
+       
+        
+        //IWebDriver driver new FirefoxDriver(); 
+         
+        
+
+
+    }
+}
